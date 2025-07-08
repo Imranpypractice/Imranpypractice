b159b2037393317c72fbb1d060e7854464c8c3e1
Imran-git-student <imran-git-email2rockyabc@gmail.com>
2025-07-09 02:55:02 +0530

    Your message here

--- s3bucket.py
+++
@@ -1,7 +0,0 @@
-import boto3
-
-s3 = boto3.client('s3', region_name='us-east-1')
-
-bucket_name = 'my-emranbucket12345678'
-
-s3.create_bucket(Bucket=bucket_name)  # ✅ No LocationConstraint for us-east-1
